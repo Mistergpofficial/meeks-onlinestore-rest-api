88d222cca64a7730fa4b4bbb79bd235a119cb32f
chuky4real <ehuriahgodspower@gmail.com>
2019-02-22 00:01:07 +0100

    latest commit to heroku

--- api/routes/order.js
+++ api/routes/order.js
@@ -112,8 +112,8 @@ router.get('/:orderId', (req, res, next) => {
 
 
 
-router.delete('/:orderId/delete', function (req, res,next) {
-    const id = req.params.orderId;
+router.post('/:id/delete', function (req, res,next) {
+    const id = req.params.id;
       Order.findById(id)
     .exec()
     .then(doc => {
