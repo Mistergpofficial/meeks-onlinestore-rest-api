51e8f20830c7a138372ea3a38e4b99148bd909a2
chuky4real <ehuriahgodspower@gmail.com>
2019-02-21 19:33:35 +0100

    latest commit to heroku

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-var cors = require('cors')
+//var cors = require('cors')
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -42,8 +42,24 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 //     }
 // });
 
-app.options('*', cors());
-app.use(cors());
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
+//app.options('*', cors());
+//app.use(cors());
 
 // Routes which should handle requests
 app.use('/user', userRoutes);
