e8f25aa218e87a886be0931918ece0c948153641
chuky4real <ehuriahgodspower@gmail.com>
2019-02-21 16:12:18 +0100

    latest commit to heroku

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use((req, res, next) => { 
     res.header('Access-Control-Allow-Origin', '*'); 
-    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type,Content-Length, Authorization, Accept,X-Requested-With,XMLHttpRequest');
+    res.header('Access-Control-Allow-Headers', '*');
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
