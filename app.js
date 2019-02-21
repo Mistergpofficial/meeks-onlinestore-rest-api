ca824295e27fb41db4c16ffa43d4cbd78b61a5c2
chuky4real <ehuriahgodspower@gmail.com>
2019-02-21 23:24:11 +0100

    latest commit to heroku

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 // });
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://meeks-onlinestore-client.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
